ff5945f5ef5dc8d1d827b12964bd5591fbb2c77a
Doktatech <Doktatech2@gmail.com>
2019-03-04 16:16:23 +0300

    define and assign a totalSum variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
+var initialScore =0;
 $(document).ready(function() {
     $("#roll").click(function(){
         var score= document.getElementById ('score');
         var remark = document.getElementById ('remark')
-        let rollValue = Math.floor( Math.random() * 6 ) + 1
+        let rollValue = Math.floor( Math.random() * 6 ) + 1        
         score.innerHTML=rollValue;
-        remark.innerHTML = 'You rolled ' + rollValue+ '.'+' Continue or Hold?';
+        var totalSum= initialScore+=rollValue;        
+        
+        if (rollValue===1){
+            remark.innerHTML ='You rolled a ' + rollValue +'.'+ ' Please hand over'
+            remark2.innerHTML= "Total score discarded"
+        }else{
+            remark.innerHTML = 'You rolled a ' + rollValue+ '.'+' Continue or Hold?';
+            remark2.innerHTML = totalSum;
+        }
+              
     $("#hold").click(function(){
-
+      remark.innerHTML=('You passed the turn to the next player');
 
     });
 
