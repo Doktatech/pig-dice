d38c36034ba6b03beebfe19db04b91dce9185dc8
Doktatech <Doktatech2@gmail.com>
2019-03-05 08:52:11 +0300

    create a constructor function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 var initialScore =0;
+function Player (Player1, Player2){
+    this.firstPlayer =Player1;
+    this.secondPlayer= Player2;
+}
 
 $(document).ready(function() {
     $("#roll").click(function(){
@@ -10,8 +14,8 @@ $(document).ready(function() {
             totalScore=initialScore+=rollValue;
         }    
        if (rollValue!==1){
-            remark.innerHTML ='Hold or Continue?';
-            remark2.innerHTML ='You total score is ' + totalScore;
+            remark.innerHTML ='You rolled '+rollValue+'.'+'Hold or Continue?';
+            remark0.innerHTML ='You total score is ' + totalScore;
             
         }else{
             remark.innerHTML = 'You rolled a ' + rollValue+ '.'+' Please hand over';
@@ -23,8 +27,23 @@ $(document).ready(function() {
         }
               
     $("#hold").click(function(){
-        var hold=document.getElementById('hold');
+        var Player1=document.getElementById('Player1');
         remark.innerHTML='You held.Please pass the Die'
+    //     if (rollValue!==1) {
+    //         totalScore1=initialScore+=rollValue;
+    //     }    
+    //    if (rollValue!==1){
+    //         remark.innerHTML ='Hold or Continue?';
+    //         remark.innerHTML ='You total score is ' + totalScore;
+            
+    //     }else{
+    //         remark.innerHTML = 'You rolled a ' + rollValue+ '.'+' Please hand over';
+    //         //document.getElementById("roll").disabled = true;
+    //     }
+    //     if (totalScore>=100){
+    //         remark.innerHTML=('Hurray!!!!You WON the Game.Refresh to Start');
+    //         document.getElementById("roll").disabled = true;
+    //     }
       
     });
 
